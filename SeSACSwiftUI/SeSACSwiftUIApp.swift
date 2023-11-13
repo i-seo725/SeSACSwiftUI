83584a732bd2167af00cbf7f432cc41c47412eb2
i-seo725 <hjing719@gmail.com>
2023-11-13 09:43:47 +0900

    Initial Commit

---
+++ SeSACSwiftUI/SeSACSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACSwiftUIApp.swift
+//  SeSACSwiftUI
+//
+//  Created by 이은서 on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSACSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
