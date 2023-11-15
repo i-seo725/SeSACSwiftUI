55a46432b8fd99ce82378537c310f1ab491faabe
i-seo725 <hjing719@gmail.com>
2023-11-15 18:25:49 +0900

    11/14 SwiftUI 화면 전환, Vstack vs LazyVstack

---
+++ HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SeSACSwiftUI
+//
+//  Created by 이은서 on 11/15/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
